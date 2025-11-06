c5e0123d6fdc81fa2193f8bbf720890aaa6eb39e
stripodi <simone.tripodi@terradue.com>
2025-11-06 19:06:25 +0100

    feat: relicensing

--- src/pygeofilter_aeronet/evaluator.py
+++ src/pygeofilter_aeronet/evaluator.py
@@ -1,3 +1,17 @@
+# Copyright 2025 Terradue
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import csv
 import os
 import json
@@ -9,7 +23,7 @@ from pygeofilter import ast, values
 from pygeofilter.backends.evaluator import Evaluator, handle
 from pygeofilter.parsers.cql2_json import parse as json_parse
 from pygeofilter.util import IdempotentDict
-from typing import Dict, Mapping, Optional, Sequence
+from typing import Mapping, Optional, Sequence
 
 
 def read_aeronet_site_list(filepath: str) -> Sequence[str]:
